01413ed2685a776fe26432e1243c58123093dfb9
Gabriel Ignacio Báez <gabriel.ignacio.baez@gmail.com>
2025-05-06 20:43:38 -0400

    Se configura ruta de acceso

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,4 @@
 export const environment = {
-  production: false,
-  // apiUrl: 'http://localhost:8080'
-  // apiUrl: 'https://13db-186-189-103-232.ngrok-free.app'
-  apiUrl: 'https://abcc-186-189-103-232.ngrok-free.app'
+  production: true,
+  apiUrl: 'https://b0f3-186-189-95-84.ngrok-free.app'
 };
